432c435d85fe679fd64292cfb1f5134ba8b721a0
Toufique Hasan <toufiquehasan07@gmail.com>
2023-04-08 11:44:29 +0600

    General ask view implemented and view name updated

--- src/navigators/drawer-navigator.js
+++ src/navigators/drawer-navigator.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Home from '../views';
-import About from '../views/about';
+import GeneralAsk from '../views/general-ask';
 import CalculateFitra from '../views/calculate-fitra';
-import {CALCULATE_FITRA_VIEW, HOME_VIEW, ABOUT_VIEW} from '../constants';
+import {CALCULATE_FITRA_VIEW, HOME_VIEW, GENERAL_ASK} from '../constants';
 
 const Drawer = createDrawerNavigator();
 
@@ -12,7 +12,7 @@ function DrawerNavigator() {
     <Drawer.Navigator>
       <Drawer.Screen name={HOME_VIEW} component={Home} />
       <Drawer.Screen name={CALCULATE_FITRA_VIEW} component={CalculateFitra} />
-      <Drawer.Screen name={ABOUT_VIEW} component={About} />
+      <Drawer.Screen name={GENERAL_ASK} component={GeneralAsk} />
     </Drawer.Navigator>
   );
 }
---
+++ src/views/general-ask.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+const GeneralAsk = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.contents}>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>
+            আপনার নির্বাচিত খাদ্য দ্রব্য "___", যার এক সা'আ অনুযায়ী রাসুল (সাঃ)
+            ফিতরা দিতেন।
+          </Text>
+        </View>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>
+            উক্ত খাদ্য দ্রব্যের এক সা'আ এর মূল্য অনুযায়ী
+          </Text>
+          <Text style={styles.text}>___ জনের জন্য আপনাকে</Text>
+          <Text style={styles.text}>___ টাকা ফিতরা দিতে হবে।</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1.0,
+    backgroundColor: '#FAF1E6',
+  },
+  contents: {
+    alignItems: 'center',
+    marginTop: '20%',
+  },
+  textWrapper: {
+    marginVertical: '10%',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+});
+export default GeneralAsk;
