7dcef45a0dadea249994d023f5f376689a2ba103
Toufique Hasan <toufiquehasan07@gmail.com>
2023-04-08 14:59:13 +0600

    functional design of result vie

--- src/views/result.js
+++ src/views/result.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
-const Result = () => {
+const Result = ({route}) => {
+  const {item, result, noOfPerson} = route.params;
+
   return (
     <View style={styles.container}>
       <View style={styles.contents}>
         <View style={styles.textWrapper}>
           <Text style={styles.text}>
-            আপনার নির্বাচিত খাদ্য দ্রব্য "___", যার এক সা'আ অনুযায়ী রাসুল (সাঃ)
-            ফিতরা দিতেন।
+            আপনার নির্বাচিত খাদ্য দ্রব্য
+            <Text style={styles.hint}> {item.title} </Text>, যার{' '}
+            <Text style={styles.hint}>
+              {item.id == 0 || item.id == 1 ? 'আধা' : 'এক'}
+            </Text>{' '}
+            সা'আ অনুযায়ী রাসুল (সাঃ) ফিতরা দিতেন।
           </Text>
         </View>
         <View style={styles.textWrapper}>
           <Text style={styles.text}>
             উক্ত খাদ্য দ্রব্যের এক সা'আ এর মূল্য অনুযায়ী
           </Text>
-          <Text style={styles.text}>___ জনের জন্য আপনাকে</Text>
-          <Text style={styles.text}>___ টাকা ফিতরা দিতে হবে।</Text>
+          <Text style={styles.text}>
+            <Text style={styles.hint}> {noOfPerson} </Text>জনের জন্য আপনাকে
+          </Text>
+          <Text style={styles.text}>
+            <Text style={styles.hint}> {result} </Text>টাকা ফিতরা দিতে হবে।
+          </Text>
         </View>
       </View>
     </View>
@@ -39,6 +49,12 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '500',
     textAlign: 'center',
+  },
+
+  hint: {
+    color: 'tomato',
+    fontSize: 22,
+    fontWeight: 'bold',
   },
 });
 export default Result;
